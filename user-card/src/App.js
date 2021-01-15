afcf7e95eb36715704ad22103e371e47b07c2564
ChadDiaz <chadcdiaz@gmail.com>
2021-01-15 15:49:37 -0500

    CDM initialized inside of App.js

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+import UserCard from './components/UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log('cd: App.js: App: constructor() -> was ran')
+    this.state= {
+      userCard: {},
+      userFollowing: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/ChadDiaz')
+    .then((res) => {
+      console.log("cd: App.js: App: CDM: user data", res)
+      this.setState = {
+        userCard: res.data,
+      }
+    }).catch((err) => console.log('err user data', err.message))
+    axios.get('https://api.github.com/users/ChadDiaz/following')
+    .then((res) => {
+      console.log("cd: App.js: App: CDM: followers data", res)
+      this.setState = {
+        userFollowing: res.data
+      }
+    }).catch((err) => console.log("err followers data", err.message))
+  }
+  
+  render() {
+    return(
+      <div>
+        <UserCard />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ user-card/src/components/UserCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserCard = () => {
+    return (
+        <div>
+            <p>Hi World!</p>
+        </div>
+    )
+}
+
+export default UserCard
