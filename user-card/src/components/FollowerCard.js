caa3ea2d2633e59d47c8486246c09ef2c9615fcd
ChadDiaz <chadcdiaz@gmail.com>
2021-01-17 17:56:26 -0500

    let us see if this revamp works

--- user-card/src/components/FollowerCard.js
+++ user-card/src/components/FollowerCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-export const FollowerCard = () => {
+export const FollowerCard = (props) => {
     return (
-        <div>
-            
-        </div>
+        <li id={`follower-${props.user.id}`}>
+            <img srd={props.user.avatar_url} alt={props.user.name} />
+            <div>
+                <div>
+                    <p>{props.user.login}</p>
+                </div>
+                <hr/>
+                <a href={props.user.html_url} target='blank' rel='noreferrer'>Visit Profile</a>
+            </div>
+        </li>
     )
 }
--- user-card/src/components/Followers.js
+++ user-card/src/components/Followers.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import FollowerCard  from './FollowerCard'
 
-export const Followers = () => {
+export const Followers = (props) => {
     return (
         <div>
-            
+            <h2>{props.followers.length > 0 ? `${props.followers.length} Followers` : "No Followers"}</h2>
+            <ul>
+                {props.followers.length > 0 && props.followers.map((user) => {
+                    return <FollowerCard user={user} key={user.id} />
+                })}
+            </ul>
         </div>
     )
 }
